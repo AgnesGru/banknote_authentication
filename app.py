0155f167c03675144b9ae1148849ef3dcb356023
AG <agnieszka.gruszecka@gmail.com>
2020-11-28 13:02:50 +0100

    some additions

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 # create Flask app
-from flask import Flask, request, render_template
-import pandas as pd
-import numpy as np
 import pickle
+import numpy as np
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 classifier = pickle.load(open('classifier.pkl', 'rb'))
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict', methods = ['POST'])
+
+@app.route('/predict', methods=['POST'])
 def predict():
     int_features = [int(x) for x in request.form.values()]  # dla wszystkich danych z formularza html
     final_features = [np.array(int_features)] # zrób z nich macierz poziomą
@@ -20,14 +21,14 @@ def predict():
     # curtosis = request.args.get('curtosis')
     # entropy = request.args.get('entropy')
     # prediction = classifier.predict([[variance, skewness, curtosis, entropy]])
-    prediction = classifier.predict(final_features)  # przekarz te dane do klasyfikatora i wywołaj metodę predict
+    prediction = classifier.predict(final_features)  # przekazac te dane do klasyfikatora i wywołaj metodę predict
     prediction = round(prediction[0], 0)
     if prediction == 1:
         prediction = 'Bank note'
     else:
         prediction = 'Fake note'
 
-    return render_template("index.html", result = 'It is a {}'.format(str(prediction)))
+    return render_template("index.html", result='It is a {}'.format(str(prediction)))
     # return f'The predicted value is {str(prediction)}'
 
 # @app.route('/predict_file', methods=['POST'])
@@ -35,6 +36,7 @@ def predict():
 #     df_test = pd.read_csv(request.files.get('file'))
 #     prediction = classifier.predict(df_test)
 #     return f'The predicted values for the csv is {str(list(prediction))}'
+
 
 if __name__=='__main__':
     app.run(debug=True)
--- model_training.py
+++ model_training.py
@@ -5,28 +5,27 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pickle
 
-df=pd.read_csv('BankNoteAuthentication.csv')
+df = pd.read_csv('BankNoteAuthentication.csv')
 
 # print(df)
 
-X=df.iloc[:,:-1]
-y=df.iloc[:,-1]
-
+X = df.iloc[:, :-1]
+y = df.iloc[:, -1]
 
 # print(X.head(5))
 # print(y.head(5))
 
 
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
-### Implement Random Forest classifier
-classifier=RandomForestClassifier()
-classifier.fit(X_train,y_train)
+# ## Implement Random Forest classifier
+classifier = RandomForestClassifier()
+classifier.fit(X_train, y_train)
 
-## Prediction
-y_pred=classifier.predict(X_test)
+# # Prediction
+y_pred = classifier.predict(X_test)
 
-score=accuracy_score(y_test,y_pred)
+score = accuracy_score(y_test, y_pred)
 # print(score)
 
 # ### Create a Pickle file using serialization
